b2e99a17486cf106fc6cca90495e35b9b85e273e
Oomkar Pandit <lolasoftware@gmail.com>
2022-04-19 12:55:54 +0100

    Updates

---
+++ precache-manifest.b42fdf80ead609f537ad8944c964becd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e5b797b9b8a075242ce",
+    "url": "/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "0e5b797b9b8a075242ce",
+    "url": "/static/js/main.0e5b797b.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "299faf9390feffb5943c",
+    "url": "/static/js/runtime~main.299faf93.js"
+  },
+  {
+    "revision": "61bd3f8533d06e271a377f5fa4202cc0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6836caa23f705a5a759b157e7b255e2.js"
+  "/precache-manifest.b42fdf80ead609f537ad8944c964becd.js"
 );
 
 workbox.clientsClaim();
